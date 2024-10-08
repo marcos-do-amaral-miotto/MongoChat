29018bfcce6b6ca763dda5c5968bdba4d371e6f3
MARCOS DO AMARAL MIOTTO <marcos.am3@puccampinas.edu.br>
2024-10-08 11:47:46 -0300

    Requirements.txt creation

--- databases/mongohandler.py
+++ databases/mongohandler.py
@@ -1,15 +1,11 @@
 from pymongo import MongoClient
 
+
 class MongoHandler:
-    def __init__(self, connection_string = None, database_name = None):
-        if connection_string is None:
-            self.connection_string = "mongodb+srv://main-user:database@aulas.sj2sb.mongodb.net/?retryWrites=true&w=majority&appName=Aulas"
-        else:
-            self.connection_string = connection_string
-        if database_name is None:
-            self.database_name = "mongo_chat"
-        else:
-            self.database_name = database_name
+    def __init__(self, connection_string="mongodb+srv://main-user:database@aulas.sj2sb.mongodb.net/?retryWrites"
+                                         "=true&w=majority&appName=Aulas", database_name="mongo_chat"):
+        self.connection_string = connection_string
+        self.database_name = database_name
         self.client = None
         self.database = None
 
@@ -24,9 +20,9 @@ class MongoHandler:
     def authenticate(self, email, password) -> bool:
         try:
             users = self.database["users"]
+            for user in users.find():
+                if user["email"] == email and user["password"] == password:
+                    return True
+            return False
         except Exception as e:
-            print(f"Erro de conexão com o Banco de Dados: {e}")
-        for user in users.find():
-            if user["email"] == email and user["password"] == password:
-                return True
-        return False
+            print(f"Erro de conexão com o Banco de Dados: {e}\n")
